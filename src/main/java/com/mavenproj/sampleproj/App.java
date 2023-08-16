95f09b3a89dda1360f3f47e67115d62a074c7cd7
Sowmiyagreens <142341072+Sowmiyagreens@users.noreply.github.com>
2023-08-16 15:16:19 +0530

    Add files via upload

---
+++ src/main/java/com/mavenproj/sampleproj/App.java
@@ -0,0 +1,13 @@
+package com.mavenproj.sampleproj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
